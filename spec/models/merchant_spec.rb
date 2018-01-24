fccfc69c15a879d0a32b02a2896b9b14c92b1fb9
memcmahon <mcmahon.meganelizabeth@gmail.com>
2018-01-24 16:17:44 -0700

    Adds merchant seed

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -1,4 +1,6 @@
+require_relative '../spec_helper'
 require 'pry'
+require 'csv'
 
 RSpec.describe Merchant do
   describe "Validations" do
@@ -14,4 +16,18 @@ RSpec.describe Merchant do
       expect(merchant).to_not be_valid
     end
   end
+
+  describe "Database Builder" do
+    it "loads csv files" do
+      merchants = CSV.open("data/merchants.csv", headers: true, header_converters: :symbol)
+      merchants.each do |row|
+        Merchant.create(id:         row[:id],
+                        name:       row[:id],
+                        created_at: row[:created_at],
+                        updated_at:  row[:updated_at])
+      end
+      expect(Merchant.count).to eq 475
+    end
+  end
+
 end
