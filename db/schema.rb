3485adfbb75667d1328a076f4e3de3cfefeda440
amj133 <amj@vt.edu>
2018-01-23 18:19:15 -0700

    table is created with test prepend

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180124000927) do
+ActiveRecord::Schema.define(version: 20180124005441) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "merchants", id: false, force: :cascade do |t|
-    t.integer "id"
+  create_table "merchants", force: :cascade do |t|
     t.text "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -3,8 +3,8 @@ require 'pry'
 RSpec.describe Merchant do
   describe "Validations" do
     it "is invalid without a name" do
-      merchant = Merchant.create(created_at: "2010-12-10", updated_at: "2011-12-04")
-
+      merchant = Merchant.create(name: "Sally")
+      binding.pry
       expect(merchant).to_not be_valid
     end
   end
