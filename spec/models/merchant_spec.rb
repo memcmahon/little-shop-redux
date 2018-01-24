1a66d3c5161cfff4742c9a1b7079e96a56a06c22
amj133 <amj@vt.edu>
2018-01-23 18:43:08 -0700

    it_1 validation tests complete

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -2,9 +2,15 @@ require 'pry'
 
 RSpec.describe Merchant do
   describe "Validations" do
-    it "is invalid without a name" do
+    it "is invalid without an  id" do
       merchant = Merchant.create(name: "Sally")
-      binding.pry
+
+      expect(merchant).to_not be_valid
+    end
+
+    it "is invalid without a name" do
+      merchant = Merchant.create(id: 1)
+
       expect(merchant).to_not be_valid
     end
   end
