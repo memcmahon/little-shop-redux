8781282fb64c6099a396c3ca965bf49d01473240
memcmahon <mcmahon.meganelizabeth@gmail.com>
2018-01-24 17:00:02 -0700

    Adds CRUD functionality for category

--- app/controllers/little_shop_app.rb
+++ app/controllers/little_shop_app.rb
@@ -50,35 +50,35 @@ class LittleShopApp < Sinatra::Base
     erb :"categories/index"
   end
 
-  # get '/merchants/new' do
-  #   erb :"merchants/new"
-  # end
-  #
-  # get '/merchants/:id' do
-  #   @merchant = Merchant.find_by(id: params[:id])
-  #   erb :"merchants/show"
-  # end
-  #
-  # get '/merchants/:id/edit' do
-  #   @merchant = Merchant.find_by(id: params[:id])
-  #   erb :"merchants/edit"
-  # end
-  #
-  # post '/merchants' do
-  #   Merchant.create(params[:merchant])
-  #   redirect '/merchants'
-  # end
-  #
-  # put '/merchants/:id' do
-  #   merchant = Merchant.find_by(id: params[:id])
-  #   merchant.update(name: params[:merchant].values.first)
-  #   redirect "/merchants/#{merchant.id}"
-  # end
-  #
-  # delete '/merchants/:id' do
-  #   merchant = Merchant.find_by(id: params[:id])
-  #   merchant.destroy
-  #   redirect '/merchants'
-  # end
+  get '/categories/new' do
+    erb :"categories/new"
+  end
+
+  get '/categories/:id' do
+    @category = Category.find_by(id: params[:id])
+    erb :"categories/show"
+  end
+
+  get '/categories/:id/edit' do
+    @category = Category.find_by(id: params[:id])
+    erb :"categories/edit"
+  end
+
+  post '/categories' do
+    Category.create(params[:category])
+    redirect '/categories'
+  end
+
+  put '/categories/:id' do
+    category = Category.find_by(id: params[:id])
+    category.update(name: params[:category].values.first)
+    redirect "/categories/#{category.id}"
+  end
+
+  delete '/categories/:id' do
+    category = Category.find_by(id: params[:id])
+    category.destroy
+    redirect '/categories'
+  end
 
 end
