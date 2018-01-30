4f8d58fe4d9844360322f3c252a5d4f97de1f00e
memcmahon <mcmahon.meganelizabeth@gmail.com>
2018-01-30 11:29:18 -0700

    Adds implementation for merchant with most items

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -4,4 +4,19 @@ class Merchant < ActiveRecord::Base
   validates :id, uniqueness: true
 
   has_many :items
+
+  def self.item_count_per_merchant_id
+    select("merchants.*, items.*")
+      .joins(:items)
+      .group(:merchant_id)
+      .count(:items)
+  end
+
+  def self.has_most_items
+    merchant_id = item_count_per_merchant_id.max_by do |merchant_id, item_count|
+      item_count
+    end[0]
+
+    find(merchant_id)
+  end
 end
