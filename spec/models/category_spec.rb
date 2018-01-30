b00a36e784437c4f6900e8bbc06ce08ca382fa94
amj133 <amj@vt.edu>
2018-01-29 17:13:59 -0700

    category average price test written

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -21,4 +21,38 @@ RSpec.describe Category do
       expect(Category.first.name).to eq "Sleepy"
     end
   end
+
+  describe "Has metric functionality" do
+    it "calculates average price of item by category" do
+      expensive = Category.create(name: 'expensive')
+      cheap = Category.create(name: 'cheap')
+      item_1 = Item.create(title: "soggy socks",
+                           description: "yikes my feet are wet!",
+                           price: 1023,
+                           image: "https://upload.wikimedia.org/wikipedia/commons/a/ab/SnowWhite44.jpg",
+                           merchant_id: 12334145,
+                           category_id: 1)
+      item_2 = Item.create(title: "ripped jeans",
+                           description: "They'll make you really cool, bro.",
+                           price: 2099,
+                           image: "https://upload.wikimedia.org/wikipedia/commons/a/ab/SnowWhite44.jpg",
+                           merchant_id: 12334148,
+                           category_id: 2)
+      item_3 = Item.create(title: "oxford polo",
+                           description: "makes you look smart!",
+                           price: 1249,
+                           image: "https://upload.wikimedia.org/wikipedia/commons/a/ab/SnowWhite44.jpg",
+                           merchant_id: 12334146,
+                           category_id: 1)
+      item_4 = Item.create(title: "leather arm cuff",
+                          description: "They'll make you reallllllllllllly cool, bro.",
+                          price: 3328,
+                          image: "https://upload.wikimedia.org/wikipedia/commons/a/ab/SnowWhite44.jpg",
+                          merchant_id: 12334147,
+                          category_id: 2)
+
+      expect(Category.average_price(1)).to eq(11.36)
+      expect(Category.average_price(2)).to eq(27.14)
+    end
+  end
 end
